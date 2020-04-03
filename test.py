f2e16f28f80c1f93981f2658b7b1e188618818b3
Gregory Brown <gsb5067@gmail.com>
2020-04-03 18:50:44 +0000

    new test file

--- test.py
+++ test.py
@@ -3,19 +3,11 @@ from uploader import ElasticSearchUploader
 
 
 def main() -> None:
-    sc: GNMIManager = GNMIManager("<IP>", "<Username>", "<Password>", "<Port>", "<Pem file>")
-    sc.connect()
-    if sc.is_connected:
-        get_complete, response = sc.get_config()
-        if get_complete:
-            es = ElasticSearchUploader("<ES IP>","<ES Port>")
-            if es.upload_config(response):
-        set_request = es.download("<Hostname>", "<Version>")
-        set_complete, response = sc.set(set_request.update_request)
-        
+    with GNMIManager("II11-5504-Daenerys.pem", "10.8.70.41", "root", "lablab", "57400", "yang-keys-663.txt") as manager:
+        responses = manager.get("JSON_IETF", ["Cisco-IOS-XR-ip-ntp-oper:ntp"])
+        for response in responses:
+            print(response)
 
-         
 
-                
 if __name__ == '__main__':
     main()
